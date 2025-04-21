0854f6b2bcf01b56a4e0a9c23f4121c0281b10c2
agam <agam.w017@gmail.com>
2025-04-21 12:05:53 +0000

    fix, using nvm for node

--- scripts/setup-unix.sh
+++ scripts/setup-unix.sh
@@ -146,11 +146,11 @@ verify_node() {
       fi
     fi
   else
-    echo "Installing Node.js using pnpm..."
-    sudo pnpm install -g n
-    sudo n latest
-    export PATH="/usr/local/bin:$PATH"
-    hash -r
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+      # in lieu of restarting the shell
+      \. "$HOME/.nvm/nvm.sh"
+      # Download and install Node.js:
+      nvm install 23
   fi
 }
 
